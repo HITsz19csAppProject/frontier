c5d0fae3029d53a39ae9df3a18301403c54350bd
LaiYUei <974152344@qq.com>
2020-07-17 16:08:14 +0800

    commit

--- app/src/main/java/com/example/chatting/FifthActivity.java
+++ app/src/main/java/com/example/chatting/FifthActivity.java
@@ -9,16 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.CenterCrop;
-import com.example.chatting.R.drawable;
 import com.example.chatting.ui.main.LoginActivity;
-
-import jp.wasabeef.glide.transformations.BlurTransformation;
-import jp.wasabeef.glide.transformations.CropCircleTransformation;
-
-import static com.example.chatting.R.drawable.*;
-import static com.example.chatting.R.drawable.ic_launcher_background;
 
 public class FifthActivity extends AppCompatActivity {
     Button cancel;
@@ -27,7 +18,7 @@ public class FifthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fifth);
+        setContentView(R.layout.activity_center);
         cancel=findViewById(R.id.cancel);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/chatting/MainActivity.java
+++ app/src/main/java/com/example/chatting/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.chatting;
 
-<<<<<<< HEAD
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,26 +15,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
-
-    private long exitTime = 0;
-    private Handler mHandler = new Handler();
-    private Runnable mFinish = new Runnable() {
-        @Override
-        public void run() {
-            finish();
-        }
-    };
-=======
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
@@ -54,22 +41,19 @@ import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<range> rangeList = new ArrayList<>();
-    private List<news> newsList=new ArrayList<>();
-    private Button add;
-    private Button notice;
-    private Button myself;
-    private NewsAdapter adapter1;
-    private Button btn_login;
-    private TextView et_username;
-    private TextView et_password;
->>>>>>> 57a7597d622aeacff8e3aff3b8bf31bc4cc297a6
+    private long exitTime = 0;
+    private Handler mHandler = new Handler();
+    private Runnable mFinish = new Runnable() {
+        @Override
+        public void run() {
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -90,171 +74,124 @@ public class MainActivity extends AppCompatActivity {
         }
         //return super.onKeyDown(keyCode, event);
         return false;
-=======
-
-        Bmob.initialize(this,"0c073307ca22bf3e1268f1f70bef2941");
-
-        addControl();
-        addLogin();
-
-        add = findViewById(R.id.add);
-        notice=findViewById(R.id.notice);
-        myself=findViewById(R.id.myself);
-        notice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ForthActivity.class);
-                startActivityForResult(intent, 1);
-            }
-        });
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
-                startActivityForResult(intent, 1);
-            }
-        });
-        myself.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,FifthActivity.class);
-                startActivityForResult(intent, 1);
-            }
-        });
-
-        initRange();
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(layoutManager);
-        RangeAdapter adapter = new RangeAdapter(rangeList);
-        recyclerView.setAdapter(adapter);
-
-        initNews();
-        adapter1 = new NewsAdapter(MainActivity.this, R.layout.news, newsList);
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter1);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                news news = newsList.get(position);
-                Intent intent = new Intent(MainActivity.this, secondActivity.class);
-                String news_headline = news.getHeadline();
-                String news_writer = news.getWriter();
-                String news_context = news.getContext();
-                intent.putExtra("extra_headline", news_headline);
-                intent.putExtra("extra_writer", news_writer);
-                intent.putExtra("extra_context", news_context);
-                startActivity(intent);
-            }
-        });
-    }
-        @Override
-        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
-            super.onActivityResult(requestCode, resultCode, data);
-            switch (requestCode) {
-                case 1:
-                    if (resultCode == RESULT_OK) {
-                        String myheadline = data.getStringExtra("headline_return");
-                        String mycontext = data.getStringExtra("context_return");
-                        news M = new news(myheadline, "me", mycontext);
-                        newsList.add(M);
-                        refresh(adapter1);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
 
 
-
-    private void initNews() {
-        for(int i=0;i<2;i++)
-        {
-            news N1=new news("Apple","John","I love apples");
-            news N2=new news("Grape","JoJo","I love grapes");
-            news N3=new news("Tomato","Lisa","I love tomatoes");
-            news N4=new news("Cherry","Lina","I love cherry");
-            news N5=new news("Strawberry","Jack","I love strawberries");
-            news N6=new news("Pear","Angel","I love pear");
-
-            newsList.add(N1);
-            newsList.add(N2);
-            newsList.add(N3);
-            newsList.add(N4);
-            newsList.add(N5);
-            newsList.add(N6);
-        }
-    }
-
-    private void initRange() {
-        for(int i=0;i<1;i++)
-        {
-            range time = new range("时间");
-            range study=new range("学习");
-            range talk=new range("闲聊");
-            range sports=new range("运动");
-            range games=new range("游戏");
-            range songs=new range("唱歌");
-            range poem=new range("诗歌");
-            range life=new range("生活");
-
-            rangeList.add(time);
-            rangeList.add(study);
-            rangeList.add(talk);
-            rangeList.add(sports);
-            rangeList.add(games);
-            rangeList.add(songs);
-            rangeList.add(poem);
-            rangeList.add(life);
-        }
-    }
-
-    public void refresh(NewsAdapter adapter)
-    {
-        adapter.notifyDataSetChanged();
-    }
-
-    //bmob登录方法
-    private void addLogin() {
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String lgU = et_username.getText().toString().trim();
-                String lgp = et_password.getText().toString().trim();
-                final BmobUser bu2 = new BmobUser();
-                bu2.setUsername(lgU);
-                bu2.setPassword(lgp);
-                bu2.login(new SaveListener<BmobUser>() {
-                    @Override
-                    public void done(BmobUser bmobUser, BmobException e) {
-                        if (e == null) {
-                            //登陆成功就跳转到主页面
-                            Intent in_success = new Intent(MainActivity.this, FifthActivity.class);
-                            startActivity(in_success);
-
-                        } else {
-                            Toast.makeText(MainActivity.this, "账户名或密码不正确", Toast.LENGTH_SHORT).show();
-                            //loge(e);
-                        }
-
-                    }
-                });
-
-
-            }
-
-
-        });
-    }
-
-    private void addControl() {
-
-        et_username = (TextView) findViewById(R.id.et_username);
-        et_password = (TextView) findViewById(R.id.et_password);
-        btn_login = (Button) findViewById(R.id.btn_login);
->>>>>>> 57a7597d622aeacff8e3aff3b8bf31bc4cc297a6
+//        Bmob.initialize(this,"0c073307ca22bf3e1268f1f70bef2941");
+//
+//        addControl();
+//        addLogin();
+//
+//        add = findViewById(R.id.add);
+//        notice=findViewById(R.id.notice);
+//        myself=findViewById(R.id.myself);
+//        notice.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,ForthActivity.class);
+//                startActivityForResult(intent, 1);
+//            }
+//        });
+//        add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+//                startActivityForResult(intent, 1);
+//            }
+//        });
+//        myself.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,FifthActivity.class);
+//                startActivityForResult(intent, 1);
+//            }
+//        });
+//
+//        initRange();
+//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        recyclerView.setLayoutManager(layoutManager);
+//        RangeAdapter adapter = new RangeAdapter(rangeList);
+//        recyclerView.setAdapter(adapter);
+//
+//        initNews();
+//        adapter1 = new NewsAdapter(MainActivity.this, R.layout.news, newsList);
+//        ListView listView = (ListView) findViewById(R.id.list_view);
+//        listView.setAdapter(adapter1);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                news news = newsList.get(position);
+//                Intent intent = new Intent(MainActivity.this, secondActivity.class);
+//                String news_headline = news.getHeadline();
+//                String news_writer = news.getWriter();
+//                String news_context = news.getContext();
+//                intent.putExtra("extra_headline", news_headline);
+//                intent.putExtra("extra_writer", news_writer);
+//                intent.putExtra("extra_context", news_context);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//        @Override
+//        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+//            super.onActivityResult(requestCode, resultCode, data);
+//            switch (requestCode) {
+//                case 1:
+//                    if (resultCode == RESULT_OK) {
+//                        String myheadline = data.getStringExtra("headline_return");
+//                        String mycontext = data.getStringExtra("context_return");
+//                        news M = new news(myheadline, "me", mycontext);
+//                        newsList.add(M);
+//                        refresh(adapter1);
+//                    }
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+//
+//
+//
+//
+//    //bmob登录方法
+//    private void addLogin() {
+//        btn_login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String lgU = et_username.getText().toString().trim();
+//                String lgp = et_password.getText().toString().trim();
+//                final BmobUser bu2 = new BmobUser();
+//                bu2.setUsername(lgU);
+//                bu2.setPassword(lgp);
+//                bu2.login(new SaveListener<BmobUser>() {
+//                    @Override
+//                    public void done(BmobUser bmobUser, BmobException e) {
+//                        if (e == null) {
+//                            //登陆成功就跳转到主页面
+//                            Intent in_success = new Intent(MainActivity.this, FifthActivity.class);
+//                            startActivity(in_success);
+//
+//                        } else {
+//                            Toast.makeText(MainActivity.this, "账户名或密码不正确", Toast.LENGTH_SHORT).show();
+//                            //loge(e);
+//                        }
+//
+//                    }
+//                });
+//
+//
+//            }
+//
+//
+//        });
+//    }
+//
+//    private void addControl() {
+//
+//        et_username = (TextView) findViewById(R.id.et_username);
+//        et_password = (TextView) findViewById(R.id.et_password);
+//        btn_login = (Button) findViewById(R.id.btn_login);
     }
 
 }
