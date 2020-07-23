b08a709cf192ba0dd9a32db8e07792184870b0ec
zetian1025 <1204822360@qq.com>
2020-07-23 15:48:28 +0800

    完成校园网认证功能

--- app/src/main/java/com/example/chatting/LoginActivity.java
+++ app/src/main/java/com/example/chatting/LoginActivity.java
@@ -6,17 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.method.HideReturnsTransformationMethod;
-import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -24,17 +13,8 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
-
-import com.example.Login.temp;
+import com.example.Login.TempLoginActivity;
 import com.example.Bean.User;
-import com.example.Login.JwloginActivity;
-import com.example.Login.RegisterActivity;
-import com.example.chatting.MainActivity;
-import com.example.chatting.R;
-import com.example.backend.JwLoginActivity;
-import com.google.android.material.snackbar.Snackbar;
 
 import org.apache.log4j.Category;
 
@@ -90,73 +70,77 @@ public class LoginActivity extends AppCompatActivity {
     private void addLogin() {
         //Bmob登陆方法
         final BmobUser user = new BmobUser();
-                String lgU = register_user.getText().toString().trim();
-                String lgp = register_password.getText().toString().trim();
-                user.setUsername(lgU);
-                user.setPassword(lgp);
-                user.login(new SaveListener<BmobUser>() {
-                    @Override
-                    public void done(BmobUser bmobUser, BmobException e) {
-                        if (e == null) {
-                            //登陆成功就跳转到主页面
-                            Intent in_success = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(in_success);
+        String lgU = register_user.getText().toString().trim();
+        String lgp = register_password.getText().toString().trim();
+        user.setUsername(lgU);
+        user.setPassword(lgp);
+        user.login(new SaveListener<BmobUser>() {
+            @Override
+            public void done(BmobUser bmobUser, BmobException e) {
+                if (e == null) {
+                    //登陆成功就跳转到主页面
+                    Intent in_success = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(in_success);
 
-                        } else {//如果登陆不成功，查询该账号是否注册
-                            BmobQuery<Category> categoryBmobQuery = new BmobQuery<>();
-                            categoryBmobQuery.addWhereEqualTo("username", user.getUsername());
-                            categoryBmobQuery.findObjects(new FindListener<Category>() {
+                }
+                else {//如果登陆不成功，查询该账号是否注册
+                    BmobQuery<Category> categoryBmobQuery = new BmobQuery<>();
+                    categoryBmobQuery.addWhereEqualTo("username", user.getUsername());
+                    categoryBmobQuery.findObjects(new FindListener<Category>() {
+                    @Override
+                    public void done(List<Category> object, BmobException e) {
+                        if (e == null) {
+                            Toast.makeText(LoginActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
+                            Intent intent_LoginWRONG = new Intent(LoginActivity.this, LoginActivity.class);
+                            startActivity(intent_LoginWRONG);
+                        }
+                        else {//尝试进行注册
+                            final boolean[] ans = {false};
+                            //插入方法
+                            Thread thread = new Thread(new Runnable() {
                                 @Override
-                                public void done(List<Category> object, BmobException e) {
-                                    if (e == null) {
-                                        Toast.makeText(LoginActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
-                                        Intent intent_LoginWRONG = new Intent(LoginActivity.this, LoginActivity.class);
-                                        startActivity(intent_LoginWRONG);
-                                    } else {//尝试进行注册
-                                        final boolean[] ans = {false};
-                                        //插入方法
-                                        Thread thread = new Thread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                User tempUser = new User();
-                                                tempUser.setName(lgU);
-                                                tempUser.setPassword(lgp);
-                                                try {
-                                                    //使用了上一届学长的代码，完成了校园网认证的功能。（摊手
-                                                    ans[0] = new temp().Login(tempUser);
-                                                } catch (IOException ex) {
-                                                    ex.printStackTrace();
-                                                }
-                                            }
-                                        });
-                                        thread.start();
-                                        if(ans[0]) {
-                                            user.signUp(new SaveListener<BmobUser>() {
-                                                @Override
-                                                public void done(BmobUser bmobUser, BmobException e) {
-                                                    if (e == null) {
-                                                        //判断是否注册成功成功则跳转到登陆的页面
-                                                        Intent intent_register = new Intent(LoginActivity.this, MainActivity.class);
-                                                        startActivity(intent_register);
-                                                        Toast.makeText(LoginActivity.this, "添加数据成功，返回objectId为：" + user.getObjectId(), Toast.LENGTH_SHORT).show();
-                                                        //这里我们需要了解一下什么objectid就像是一个登陆校验一样只要有这个就代表着你往表里面写入的数据是成功的了。
-                                                    }
-                                                    else {
-                                                        Toast.makeText(LoginActivity.this, "用户名或者密码不能为空", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                }
-                                            });
+                                public void run() {
+                                        User tempUser = new User();
+                                        tempUser.setName(lgU);
+                                        tempUser.setPassword(lgp);
+                                        try {
+                                            //使用了上一届学长的代码，完成了校园网认证的功能。（摊手
+                                            ans[0] = new TempLoginActivity().Login(tempUser);
+                                        } catch (IOException ex) {
+                                            ex.printStackTrace();
                                         }
                                     }
-                                }
                             });
-                            /**Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                             startActivity(intent);*/
+                            thread.start();
+                            try {
+                                    thread.join();
+                                } catch (InterruptedException ex) {
+                                    ex.printStackTrace();
+                                }
+                            if(ans[0]) {
+                                user.signUp(new SaveListener<BmobUser>() {
+                                    @Override
+                                    public void done(BmobUser bmobUser, BmobException e) {
+                                        if (e == null) {
+                                            //判断是否注册成功成功则跳转到登陆的页面
+                                            Intent intent_register = new Intent(LoginActivity.this, MainActivity.class);
+                                            startActivity(intent_register);
+                                            Toast.makeText(LoginActivity.this, "添加数据成功，返回objectId为：" + user.getObjectId(), Toast.LENGTH_SHORT).show();
+                                            //这里我们需要了解一下什么objectid就像是一个登陆校验一样只要有这个就代表着你往表里面写入的数据是成功的了。
+                                        }
+                                        else {
+                                            Toast.makeText(LoginActivity.this, "用户名或者密码不能为空", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
                         }
-
                     }
-
-
+                    });
+                        /**Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                         startActivity(intent);*/
+                }
+            }
         });
     }
      private class onClick implements View.OnClickListener{
